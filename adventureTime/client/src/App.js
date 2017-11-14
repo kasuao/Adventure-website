ad86b64779154e51845abcda2ce896a50b944f7f
PilotCo <cody.pilot@outlook.com>
2017-11-13 20:39:57 -0700

    pushing the changes for the initial home page

--- adventureTime/client/src/App.js
+++ adventureTime/client/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Adventure from "./pages/Adventure";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
+import Home from "./pages/Home";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 const App = () =>
 
     <div>
-      <Adventure></Adventure>
+      <Home></Home>
+
     </div>;
 
 export default App;
