dc3d231b0c1175e51c19b0184781a1691598e325
Kelcey Asuao <kelceyasuao@gmail.com>
2017-11-15 21:17:09 -0700

    user page set up properly on backend

--- adventureTime/client/src/App.js
+++ adventureTime/client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Adventure from "./pages/Adventure";
+import User from "./pages/User";
 import Home from "./pages/Home";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
@@ -17,7 +18,7 @@ const App = () =>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/adventure" component={Adventure} />
-        <Route exact path="/user" component={user} />
+        <Route exact path="/user" component={User} />
       </Switch>
     </div>
   </Router>;
--- adventureTime/models/user.js
+++ adventureTime/models/user.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const usersSchema = new Schema({
-  _id: Schema.Types.ObjectId,
+const userSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  profilePic: { type: String, required: true },
+  profilePic: { type: String, required: false },
   about: { type: String, required: true },
   adventureLevel: { type: Number, required: true }
 });
 
-const Users = mongoose.model("Users", usersSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = Users;
+module.exports = User;
