bd8cb6185c8e5454eba12d97ca00cb59024f32c7
colebearski <cole.pedersen@gmail.com>
2017-11-16 10:59:33 -0700

    new branch | profile template

---
+++ adventureTime/client/src/components/ProfileHeader/ProfileHeader.js
@@ -0,0 +1,124 @@
+import React from "react";
+import "./ProfileHeader.css";
+
+//Create the Header of our homepage as a component.
+const ProfileHeader = props =>
+<div class="container">
+  <div className="profileHeader">
+    <h1 id="profileTitle">PROFILE PAGE</h1>
+
+    <div>
+        <h2 id="profileName">Awesome Dude 78686</h2>
+    </div>
+
+  </div>
+    
+    <div class="row">
+      <div class="col-md-8">
+
+
+        <div class="blog-post">
+          <img class="thumbnail" src="http://placehold.it/500x300" />
+
+    		<div id="aboutMe">
+        			<h3>About Me</h3>
+        	</div>
+
+      		<div class="bio">	
+          		<p>Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.</p>
+      		</div>	
+        </div>
+
+      {/* col medium 8 ending div */}
+      </div>
+
+      <div class="col-md-4">
+
+        <div class="medium-3 columns" data-sticky-container>
+        <div class="sticky" data-sticky data-anchor="content">
+          
+          <h4>Adventure Level</h4>
+
+          <div class="progress" style={{width: 250 + "px", height: 25 + "px"}}>
+  			<div class="progress-bar progress-bar-warning progress-bar-striped active" role="progressbar"
+  			aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style={{width: 40 + "%"}}>
+    		40%
+  			</div>
+		  </div>
+
+          <h4>Landscape Level</h4>
+
+          <div class="progress" style={{width: 250 + "px", height: 25 + "px"}}>
+  			<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar"
+  			aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={{width: 60 + "%"}}>
+    		60%
+  			</div>
+		  </div>
+			
+		  <h4>Difficulty Level</h4>
+
+		  <div class="progress" style={{width: 250 + "px", height: 25 + "px"}}>
+  			<div class="progress-bar progress-bar-danger progress-bar-striped active" role="progressbar"
+  			aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style={{width: 80 + "%"}}>
+    		80%
+  			</div>
+		  </div>
+
+		  <h4>Fun Level</h4>
+
+		  <div class="progress" style={{width: 250 + "px", height: 25 + "px"}}>
+  			<div class="progress-bar progress-bar-striped active" role="progressbar"
+  			aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style={{width: 90 + "%"}}>
+    		90%
+  			</div>
+		  </div>
+
+		  <h4>Wicked Level</h4>
+
+		  <div class="progress" style={{width: 250 + "px", height: 25 + "px"}}>
+  			<div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar"
+  			aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style={{width: 95 + "%"}}>
+    		95%
+  			</div>
+		  </div>
+          
+        </div>
+      </div>
+
+    {/* column ending div */}
+    </div>
+
+ {/* row ending div */}
+ </div>
+
+
+        <div class="blog-post">
+          <h3>Awesome blog post title <small>3/6/2015</small></h3>
+          <img class="thumbnail" src="http://placehold.it/850x350" />
+          <p>Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.</p>
+        </div>
+
+        <div class="blog-post">
+          <h3>Awesome blog post title <small>3/6/2015</small></h3>
+          <img class="thumbnail" src="http://placehold.it/850x350" />
+          <p>Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.</p>
+        </div>
+
+        <div class="blog-post">
+          <h3>Awesome blog post title <small>3/6/2015</small></h3>
+          <img class="thumbnail" src="http://placehold.it/850x350" />
+          <p>Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.</p>
+        </div>
+
+        </div>
+      
+
+      
+
+      
+    
+	
+ 
+ 
+
+export default ProfileHeader;
---
+++ adventureTime/client/src/pages/Profile/profile.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+//import the used components
+import Footer from "../../components/Footer";
+import HomeHeader from "../../components/HomeHeader";
+import ProfileHeader from "../../components/ProfileHeader";
+
+import CreateUserModal from "../../components/CreateUserModal";
+import "./profile.css";
+
+class Profile extends Component {
+//declare and store state variables
+	state = {
+		profiles: [],
+	};
+
+	/*
+	constructor(props){
+		super(props)
+		this.handleUserCreate = this.handleUserCreate.bind(this);
+	}
+	*/
+
+	/*this function will run when the "Create an Account" text is clicked by the user.
+		when this is clicked it will set a state variable to 1. when this state variable is 1,
+		there will be a modal popup prompting the user to sign up for the site.
+	*/
+
+	/*
+	handleUserCreate = () => {
+		this.setState({
+      createUser: true
+    })
+	};
+	*/
+
+	/*this function will run whenever the "close" button is clicked in the the "Create New User" modal.
+		what this function does is set the createUser state to false. Meaning, whenever the page renders,
+		the new user modal will not pop up (close the window).
+	*/	
+
+	/*
+	closeUserCreate = () => {
+		this.setState({
+      createUser: false
+    })
+	}
+	*/
+
+	/*
+		This function will run whenever the "submit" button is clicked in the new user modal
+		This will be a post route to send the user information to the database. 
+	*/
+	/*
+	handleCreateSubmit = () => {
+		alert("submitted");
+	}
+	*/
+
+	render() {
+		return (
+			<div>
+
+				<ProfileHeader></ProfileHeader>
+
+
+				<Footer></Footer>
+			</div>
+		);
+	}
+
+}
+
+export default Profile;
