bfe2128855ee9ed44cc153f5b045b8bc787114d2
Kara Peterson <karapeterson@karas-macbook.uconnect.utah.edu>
2017-11-29 19:57:01 -0700

    updated home page

--- adventureTime/client/src/components/HomeHeader/HomeHeader.js
+++ adventureTime/client/src/components/HomeHeader/HomeHeader.js
@@ -9,29 +9,24 @@ const HomeHeader = props =>
 	    <h1 id="siteTitle">ADVENTURE AWAITS</h1>
 	  </div>
 	  <div className="right-header-col">
-	    <form className="col s12">
-	    	<div>
-					<div id="loginEmail" className="input-field col s3">
-					  <label for="login-email"> email:</label>
-					    <input type="email" name="email" id="inputEmail"/>
-					</div>
-					<div id="loginPassword" className="input-field col s3">
-					  <label for="password-login">password:</label>
-					  <input type="password" name="password" id="inputPassword" />
-					</div>
+	    <form className="form-container">
+				<div id="loginEmail" className="input-field col s3">
+				  <label for="login-email"> email:</label>
+				  <input type="email" name="email" id="inputEmail"/>
 				</div>
-				
-				
-			
-			
-			</form>
-		</div>
-		<div className="bottom-header">
-					<p onClick={props.handleUserCreate} id="create">Create an Account</p>
+				<div id="loginPassword" className="input-field col s3">
+				  <label for="password-login">password:</label>
+				  <input type="password" name="password" id="inputPassword" />
 				</div>
 				<div className="submit-button">
-		<button id="loginSubmit" value="Login" onClick={props.handleUserLogin}>Login</button>
-	</div>
+					<button id="loginSubmit" value="Login" onClick={props.handleUserLogin}>Login</button>
+				</div>
+			</form>
+			<div className="bottom-header">
+					<p onClick={props.handleUserCreate} id="create">Create an Account</p>
+			</div>
+		</div>
+		
   </div>;
 
 export default HomeHeader;
