9140be6d411a9ff71fc54809ca052e245c4b1085
Kara Peterson <karapeterson@Karas-MacBook.local>
2017-12-01 16:22:33 -0700

    added css stuff

--- adventureTime/client/src/components/ProfileHeader/ProfileHeader.js
+++ adventureTime/client/src/components/ProfileHeader/ProfileHeader.js
@@ -13,7 +13,7 @@ const ProfileHeader = props =>
 
   </div>
     
-    <div className="row">
+    <div id="profile-info"className="row">
       <div className="col-md-8">
 
 
@@ -25,6 +25,15 @@ const ProfileHeader = props =>
 
     		<div id="aboutMe">
         			<h3>About Me</h3>
+              <p> It is a long established fact that a reader will be distracted 
+              by the readable content of a page when looking at its layout. The point 
+              of using Lorem Ipsum is that it has a more-or-less normal distribution 
+              of letters, as opposed to using 'Content here, content here', making it 
+              look like readable English. Many desktop publishing packages and web page
+               editors now use Lorem Ipsum as their default model text, and a search 
+               for 'lorem ipsum' will uncover many web sites still in their infancy. 
+                Various versions have evolved over the years, sometimes by accident,
+                 sometimes on purpose (injected humour and the like).</p>
         	</div>
 
       		<div className="bio">	
