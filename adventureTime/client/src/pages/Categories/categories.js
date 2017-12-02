827becc64670d8557613c048f300b2663c1266b7
colebearski <cole.pedersen@gmail.com>
2017-12-01 19:06:29 -0700

    fixed nav, added collapse logic

--- adventureTime/client/src/pages/Categories/categories.js
+++ adventureTime/client/src/pages/Categories/categories.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import API from "../../utils/API";
 import { Link } from "react-router-dom";
 import axios from "axios";
+
+import Nav from "../../components/Nav";
 import Footer from "../../components/Footer";
 import AdventureHeader from "../../components/AdventureHeader";
 import CategoryLayout from "../../components/CategoryLayout";
@@ -121,6 +123,9 @@ class Categories extends Component {
 	render() {
 		return(
 			<div>
+
+      <Nav>
+      </Nav>
 	
 			<AdventureHeader/>
 
